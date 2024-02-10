f996553d7dd73ae59cb6e462232d5b440f12f265
John Henry Thompson <jht9629@nyu.edu>
2024-02-10 04:52:02 -0500

    lib/1

--- examples/lobby/a_sketch.js
+++ examples/lobby/a_sketch.js
@@ -1,9 +1,12 @@
 // https://editor.p5js.org/jht9629-nyu/sketches/xxxx
 // lobby
+
 let my = {};
 
-// my.stored_devices
-// dstore_device_summary()
+// dstore_device_summary()[0].uid
+// dstore_device_summary()[0].serverValues.date_s
+// dstore_device_summary()[0].serverValues.visit_count
+// dstore_device_summary()[0].serverValues.userAgent
 
 function my_setup() {
   my.width = 400;
@@ -26,25 +29,24 @@ function setup() {
   console.log('config.projectId', config.projectId, 'configLabel', config.configLabel);
   console.log('config.configVersion', config.configVersion, 'room', my.roomName);
 
-  dstore_init();
+  dstore_init({ dstore_host_init });
 }
 
-function dstore_init() {
-  let { signInAnonymously, auth } = fb_;
-  signInAnonymously(auth)
-    .then(() => {
-      my.uid = auth.currentUser.uid;
-      console.log('dstore_init my.uid', my.uid);
-
-      dstore_device_onChild();
-    })
-    .catch((error) => {
-      console.log('dstore_init error', error);
-    });
+function dstore_host_init() {
+  console.log('dstore_host_init');
 }
 
 function draw() {
-  //
+  background(200);
+  let ds = dstore_device_summary();
+  if (!ds) return;
+  let n = ds.length;
+  let len = int(width / n);
+  let y = int(len / 2);
+  fill(0);
+  for (let x = 0; x < width; x += len) {
+    circle(x + len / 2, y, len);
+  }
 }
 
 // dstore_device_remove()
