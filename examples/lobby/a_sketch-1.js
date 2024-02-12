bd503af8599dc0e4f13427f441a0fcb2694e8911
John Henry Thompson <jht9629@nyu.edu>
2024-02-12 01:06:25 -0500

    Update a_sketch-1.js

--- examples/lobby/a_sketch-1.js
+++ examples/lobby/a_sketch-1.js
@@ -80,3 +80,23 @@ function draw() {
     }
   }
 }
+
+function downloadAction() {
+  if (!my.devices) return;
+  let str = JSON.stringify(my.devices, undefined, 2);
+  downloadToFile(my.mo_app + '-live' + '.json', str);
+}
+
+// https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
+function downloadToFile(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
