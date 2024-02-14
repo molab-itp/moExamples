52895f70b164cf5f8375a4db5d6e4cda3936ac81
John Henry Thompson <jht9629@nyu.edu>
2024-02-14 10:15:05 -0500

    p5moLibrary

--- bin/install.sh
+++ bin/install.sh
@@ -4,10 +4,12 @@ cd ${0%/*}
 # clone p5moLibrary
 # clone p5moRelease 
 
+cd ..
+
 # check for destination p5moLibrary
 dest=p5moLibrary
 if [ ! -e "$dest" ]; then
-  git clone https://github.com/molab-itp/p5moRelease.git $dest
+  git clone https://github.com/molab-itp/$dest.git $dest
 fi
 if [ ! -e "$dest" ]; then
   echo "fail to clone to $dest"
@@ -17,7 +19,7 @@ fi
 # check for destination p5moRelease
 dest=p5moRelease
 if [ ! -e "$dest" ]; then
-  git clone https://github.com/molab-itp/p5moRelease.git $dest
+  git clone https://github.com/molab-itp/$dest.git $dest
 fi
 if [ ! -e "$dest" ]; then
   echo "fail to clone to $dest"
