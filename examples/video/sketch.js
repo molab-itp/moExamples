6da5c46908c614f7926c55c40156b0ea8f4aa6a7
John Henry Thompson <jht9629@nyu.edu>
2024-03-23 14:09:41 -0400

    eff_a_slit_scan

--- examples/video/sketch.js
+++ examples/video/sketch.js
@@ -1,5 +1,5 @@
 // https://editor.p5js.org/jht9629-nyu/sketches/KeRAIMzHN
-// p5moExamples video 366
+// p5moExamples video 368
 
 let videoKit; // home for library routines
 
@@ -38,9 +38,7 @@ let a_config = {
   // the "effects" array creates a pull-down menu
   // which offers a first selection of effects added to the VideoKit library,
   // you could add some more !!!!
-  // effects: [
-  // { label: 'a_slit_scan', import_path: 'effects/eff_a_slit_scan.js' },
-  // ],
+  effects: [{ label: 'a_slit_scan', factory: eff_a_slit_scan }],
   // settings for import, will appear in the settings menu
   // SETTINGS will load a save .json file with predefined values
   // for all the settings associated with the effect
