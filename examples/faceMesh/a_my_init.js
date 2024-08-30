4d5f94ef8db8301b1916aebd52ee8810f3cad1cc
John Henry Thompson <jht9629@nyu.edu>
2024-08-30 09:58:06 -0400

    93

--- examples/faceMesh/a_my_init.js
+++ examples/faceMesh/a_my_init.js
@@ -1,7 +1,7 @@
 //
 function my_init() {
   // updated to verify change on mobile
-  my.version = '?v=92';
+  my.version = '?v=93';
   my.isRemote = 1;
   // show detailed log
   my.logLoud = 1;
--- examples/pixel-grid/a_my_init1.js
+++ examples/pixel-grid/a_my_init1.js
@@ -1,7 +1,7 @@
 //
 function my_init1() {
   // updated to verify change on mobile
-  my.version = '?v=92';
+  my.version = '?v=93';
   my.isRemote = 1;
   my.settingsFlag = 0;
   // Aspect ratio of video capture
