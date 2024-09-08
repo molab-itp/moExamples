98be92fe9a9b7e1db2b667a6eabb63924e444283
John Henry Thompson <jht9629@nyu.edu>
2024-09-08 13:11:34 -0400

    103

--- examples/photo-booth/a_my_init.js
+++ examples/photo-booth/a_my_init.js
@@ -1,7 +1,7 @@
 //
 function my_init() {
   // updated to verify change on mobile
-  my.version = '?v=102';
+  my.version = '?v=103';
   my.isRemote = 1;
   my.videoFlag = 1;
   // show detailed log
--- examples/pixel-grid/a_my_init1.js
+++ examples/pixel-grid/a_my_init1.js
@@ -1,7 +1,7 @@
 //
 function my_init1() {
   // updated to verify change on mobile
-  my.version = '?v=102';
+  my.version = '?v=103';
   my.isRemote = 1;
   my.settingsFlag = 0;
   // Aspect ratio of video capture
