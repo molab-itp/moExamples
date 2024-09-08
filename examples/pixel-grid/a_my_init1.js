1bca593e8ab7fe5f6de209b361f999b0199b3c77
John Henry Thompson <jht9629@nyu.edu>
2024-09-08 01:32:29 -0400

    99

--- examples/pixel-grid/a_my_init1.js
+++ examples/pixel-grid/a_my_init1.js
@@ -1,7 +1,7 @@
 //
 function my_init1() {
   // updated to verify change on mobile
-  my.version = '?v=98';
+  my.version = '?v=99';
   my.isRemote = 1;
   my.settingsFlag = 0;
   // Aspect ratio of video capture
